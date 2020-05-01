fcffdd0a1ef0f84734ace83090b4a807937eaa29
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-01 12:48:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,7 +58,9 @@ class Student
 
   def self.first_X_students_in_grade_10(num)
     sql = <<-SQL
-      SELECT * FROM Students LIMIT ?
+      SELECT * FROM Students 
+      WHERE grade = 10
+      LIMIT ?
       SQL
 
       DB[:conn].execute(sql).map do |row|

e5d1a162a6750d05fa97619923a449f59030b6c2
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-01 12:42:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,8 +58,7 @@ class Student
 
   def self.first_X_students_in_grade_10(num)
     sql = <<-SQL
-      SELECT *
-      FROM students
+      SELECT * FROM students
       WHERE grade = 10
       LIMIT ?
       SQL

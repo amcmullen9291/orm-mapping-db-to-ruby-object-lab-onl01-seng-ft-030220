c0a70c0caecbb3bf7ac19d8bff09b5e5ac027d86
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-01 12:16:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,17 +81,7 @@ class Student
       end.first
   end
 
-  def self.all_students_in_grade_x(grade)
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE grade = ?
-      SQL
 
-      DB[:conn].execute(sql).map do |row|
-        self.new_from_db(row)
-      end
-  end
 
   def save
     sql = <<-SQL
